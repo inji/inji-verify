4dfc9e13597d933bd824d8df56e72449fbcb69a2
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-12-12 18:36:39 +0530

    [INJIVER-1500] - update presentation_definition_uri
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- inji-verify-sdk/src/components/openid4vp-verification/OpenID4VPVerification.tsx
+++ inji-verify-sdk/src/components/openid4vp-verification/OpenID4VPVerification.tsx
@@ -94,7 +94,7 @@ const OpenID4VPVerification: React.FC<OpenID4VPVerificationProps> = ({
         if (data.authorizationDetails.presentationDefinitionUri) {
           params.set(
             "presentation_definition_uri",
-            verifyServiceUrl + data.authorizationDetails.presentationDefinitionUri
+            data.authorizationDetails.presentationDefinitionUri
           );
         } else {
           params.set(
