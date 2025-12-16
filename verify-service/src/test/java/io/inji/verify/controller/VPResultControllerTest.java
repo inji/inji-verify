0723140198c228836198b348d7147a14d41adf95
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2025-12-16 14:56:08 +0530

    [INJIVER-1489] - add test for VPWithoutProofException
    Signed-off-by: Muskan Rai <184902125+muskanrai-tw@users.noreply.github.com>

--- verify-service/src/test/java/io/inji/verify/controller/VPResultControllerTest.java
+++ verify-service/src/test/java/io/inji/verify/controller/VPResultControllerTest.java
@@ -5,8 +5,8 @@ import io.inji.verify.dto.core.ErrorDto;
 import io.inji.verify.dto.submission.VPTokenResultDto;
 import io.inji.verify.enums.ErrorCode;
 import io.inji.verify.enums.VPResultStatus;
-import io.inji.verify.exception.InvalidVpTokenException;
 import io.inji.verify.exception.VPSubmissionNotFoundException;
+import io.inji.verify.exception.VPWithoutProofException;
 import io.inji.verify.services.VCSubmissionService;
 import io.inji.verify.services.VerifiablePresentationRequestService;
 import io.inji.verify.services.VerifiablePresentationSubmissionService;
@@ -16,8 +16,10 @@ import org.mockito.Mockito;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -104,7 +106,7 @@ public class VPResultControllerTest {
 
         when(verifiablePresentationRequestService.getLatestRequestIdFor(transactionId)).thenReturn(requestIds);
         when(verifiablePresentationSubmissionService.getVPResult(requestIds, transactionId))
-                .thenThrow(new InvalidVpTokenException());
+                .thenThrow(new VPWithoutProofException());
 
         mockMvc.perform(get("/vp-result/{transactionId}", transactionId)
                 .contentType(MediaType.APPLICATION_JSON))
