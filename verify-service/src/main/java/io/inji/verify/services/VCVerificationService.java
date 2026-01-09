87d38d3814f680a095aebde73c32df806ab4e6ae
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-01-09 12:19:13 +0530

    [INJIVER-1488] - add new service verifyV2
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/services/VCVerificationService.java
+++ verify-service/src/main/java/io/inji/verify/services/VCVerificationService.java
@@ -1,9 +1,13 @@
 package io.inji.verify.services;
 
+import io.inji.verify.dto.verification.VCVerificationRequestBodyDto;
+import io.inji.verify.dto.verification.VCVerificationResultDto;
 import io.inji.verify.dto.verification.VCVerificationStatusDto;
 import io.inji.verify.exception.CredentialStatusCheckException;
 
 public interface VCVerificationService {
 
     VCVerificationStatusDto verify(String vc, String contentType) throws CredentialStatusCheckException;
+
+    VCVerificationResultDto verifyV2(VCVerificationRequestBodyDto request);
 }
