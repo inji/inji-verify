6ef8e8d8a0e3b720228bc0ef9f9caabc2aaaed35
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-01-16 17:43:36 +0530

    [INJIVER-1491] - add vp-result v2 endpoint getVPResultV2
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/controller/VPResultController.java
+++ verify-service/src/main/java/io/inji/verify/controller/VPResultController.java
@@ -2,22 +2,29 @@ package io.inji.verify.controller;
 
 import java.util.Optional;
 import io.inji.verify.dto.core.ErrorDto;
+import io.inji.verify.dto.result.VPVerificationResultDto;
+import io.inji.verify.dto.result.VerificationRequestDto;
 import io.inji.verify.dto.submission.VPTokenResultDto;
 import io.inji.verify.enums.ErrorCode;
-import io.inji.verify.exception.CredentialStatusCheckException;
-import io.inji.verify.exception.VPSubmissionNotFoundException;
-import io.inji.verify.exception.VPSubmissionWalletError;
 import io.inji.verify.exception.VPWithoutProofException;
+import io.inji.verify.exception.VPSubmissionWalletError;
+import io.inji.verify.exception.VPSubmissionNotFoundException;
+import io.inji.verify.exception.CredentialStatusCheckException;
+import io.inji.verify.exception.TokenMatchingFailedException;
 import io.inji.verify.services.VCSubmissionService;
 import io.inji.verify.services.VerifiablePresentationRequestService;
 import io.inji.verify.services.VerifiablePresentationSubmissionService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import static io.inji.verify.utils.Utils.getResponseEntityForCredentialStatusException;
 
@@ -63,6 +70,23 @@ public class VPResultController {
             } catch (CredentialStatusCheckException ex) {
                 return getResponseEntityForCredentialStatusException(ex, request);
             }
+        }
+    }
+
+    @PostMapping(path = "/v2/vp-results/{transactionId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getVPResultV2(@PathVariable String transactionId, @Valid @RequestBody VerificationRequestDto request, HttpServletRequest httpServletRequest) throws TokenMatchingFailedException {
+        List<String> requestIds = verifiablePresentationRequestService.getLatestRequestIdFor(transactionId);
+        log.info("Fetching VP result for requestId: {}", requestIds);
+        try {
+            VPVerificationResultDto resultDto = verifiablePresentationSubmissionService.getDetailVPResult(request, requestIds, transactionId);
+            return ResponseEntity.status(HttpStatus.OK).body(resultDto);
+        } catch (VPSubmissionNotFoundException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDto(ErrorCode.NO_VP_SUBMISSION));
+        } catch (VPWithoutProofException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorDto(ErrorCode.VP_WITHOUT_PROOF));
+        } catch (VPSubmissionWalletError e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorDto(e.getErrorCode(), e.getErrorDescription()));
         }
     }
 }
