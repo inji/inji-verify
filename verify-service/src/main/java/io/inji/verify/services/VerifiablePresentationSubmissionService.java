e351b88ec4df9ae80f752c02ae51b0d7cd8de1c2
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-01-16 18:00:03 +0530

    [INJIVER-1491] - add new service getDetailVPResult
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/services/VerifiablePresentationSubmissionService.java
+++ verify-service/src/main/java/io/inji/verify/services/VerifiablePresentationSubmissionService.java
@@ -1,11 +1,15 @@
 package io.inji.verify.services;
 
+import io.inji.verify.dto.result.VPVerificationResultDto;
+import io.inji.verify.dto.result.VerificationRequestDto;
 import io.inji.verify.dto.submission.VPSubmissionDto;
 import io.inji.verify.dto.submission.VPTokenResultDto;
 import io.inji.verify.exception.CredentialStatusCheckException;
+import io.inji.verify.exception.TokenMatchingFailedException;
+import io.inji.verify.exception.VPWithoutProofException;
 import io.inji.verify.exception.VPSubmissionNotFoundException;
 import io.inji.verify.exception.VPSubmissionWalletError;
-import io.inji.verify.exception.VPWithoutProofException;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
@@ -13,4 +17,6 @@ public interface VerifiablePresentationSubmissionService {
     void submit(VPSubmissionDto vpSubmissionDto);
 
     VPTokenResultDto getVPResult(List<String> requestId, String transactionId) throws VPSubmissionNotFoundException, VPSubmissionWalletError, CredentialStatusCheckException, VPWithoutProofException;
+
+    VPVerificationResultDto getDetailVPResult(@Valid VerificationRequestDto request, List<String> requestIds, String transactionId) throws VPSubmissionWalletError, TokenMatchingFailedException, VPWithoutProofException, VPSubmissionNotFoundException;
 }
