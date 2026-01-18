e1392595491a6a4460075019332a402ba773088e
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-01-18 20:18:00 +0530

    [INJIVER-1491] - add TOKEN_MATCHING_FAILED exception
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/enums/ErrorCode.java
+++ verify-service/src/main/java/io/inji/verify/enums/ErrorCode.java
@@ -13,6 +13,7 @@ public enum ErrorCode {
     NO_PRESENTATION_DEFINITION("NO_PRESENTATION_DEFINITION","No Presentation Definition found for given Presentation Definition ID."),
     DID_CREATION_FAILED("DID_CREATION_FAILED","Error while creating DID document."),
     VP_SUBMISSION_EXCEPTION("VP_SUBMISSION_EXCEPTION","Error while processing VP submission"),
+    TOKEN_MATCHING_FAILED("TOKEN_MATCHING_FAILED", "Token matching failed."),
     VP_WITHOUT_PROOF("VP_WITHOUT_PROOF", "Invalid VP Submission since VP is without proof");
 
     private final String errorCode;
