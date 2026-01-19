1753181cb68128ed92947f2750b5d45752b106bc
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-01-19 17:32:33 +0530

    [INJIVER-1491] - remove unwanted imports
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/services/VerifiablePresentationSubmissionService.java
+++ verify-service/src/main/java/io/inji/verify/services/VerifiablePresentationSubmissionService.java
@@ -4,13 +4,7 @@ import io.inji.verify.dto.result.VPVerificationResultDto;
 import io.inji.verify.dto.result.VerificationRequestDto;
 import io.inji.verify.dto.submission.VPSubmissionDto;
 import io.inji.verify.dto.submission.VPTokenResultDto;
-import io.inji.verify.exception.CredentialStatusCheckException;
-import io.inji.verify.exception.TokenMatchingFailedException;
-import io.inji.verify.exception.VPWithoutProofException;
-import io.inji.verify.exception.VPSubmissionNotFoundException;
-import io.inji.verify.exception.VPSubmissionWalletError;
 import jakarta.validation.Valid;
-
 import java.util.List;
 
 public interface VerifiablePresentationSubmissionService {
