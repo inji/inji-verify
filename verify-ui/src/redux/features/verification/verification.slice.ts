0fc08bf6bb96dfdd0ceb132aa046477b05cc1e79
Muskan Rai <184902125+muskanrai-tw@users.noreply.github.com>
2026-01-20 16:21:38 +0530

    INJIVER-1219-review-comments-fix
    Signed-off-by: Muskan Rai <184902125+muskanrai-tw@users.noreply.github.com>
    
    # Conflicts:
    #	verify-ui/src/App.tsx

--- verify-ui/src/redux/features/verification/verification.slice.ts
+++ verify-ui/src/redux/features/verification/verification.slice.ts
@@ -23,6 +23,8 @@ const verificationSlice = createSlice({
         },
         verificationInit: (state, action) => {
             state.activeScreen = VerificationSteps[state.method].Verifying;
+            state.qrReadResult = action.payload.qrReadResult;
+            state.ovp = action.payload;
         },
         verificationComplete: (state, action) => {
             state.activeScreen = VerificationSteps[state.method].DisplayResult;
