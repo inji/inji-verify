1839ef38a95ccc551a95a027fe02f13405a5c996
srikanth716 <srikanthsri7447@gmail.com>
2026-02-05 10:29:58 +0530

    [INJIVER-1541]fix:missing qrcode in svg template
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- verify-ui/src/components/Home/VerificationSection/Result/VcSvgTemplate.tsx
+++ verify-ui/src/components/Home/VerificationSection/Result/VcSvgTemplate.tsx
@@ -4,6 +4,7 @@ import { AnyVc } from "../../../../types/data-types";
 import { fetchSvgTemplate } from "../../../../utils/svg-template-utils";
 import Loader from "../../../commons/Loader";
 import DOMPurify from "dompurify";
+import QRCode from "qrcode";
 
 interface VcSvgTemplateProps {
   vc: AnyVc;
@@ -14,6 +15,26 @@ interface VcSvgTemplateProps {
 const VcSvgTemplate = ({ vc, templateUrl, onError }: VcSvgTemplateProps) => {
   const [templateContent, setTemplateContent] = useState<string>("");
   const [loader, setLoader] = useState(false);
+  const [qrCodeDataUrl, setQrCodeDataUrl] = useState<string>("");
+
+  // Generate QR code
+  useEffect(() => {
+    QRCode.toDataURL("https://verify.example.com/credential/123", {
+      width: 256,
+      margin: 2,
+      color: {
+        dark: "#000000",
+        light: "#FFFFFF"
+      }
+    })
+    .then((url) => {
+      console.log("Generated QR code:", url.substring(0, 100));
+      setQrCodeDataUrl(url);
+    })
+    .catch((err) => {
+      console.error("QR generation error:", err);
+    });
+  }, []);
 
   useEffect(() => {
     const loadTemplate = async () => {
@@ -41,7 +62,7 @@ const VcSvgTemplate = ({ vc, templateUrl, onError }: VcSvgTemplateProps) => {
     loadTemplate();
   }, [onError, templateUrl]);
 
-  if (loader) return <Loader innerBg="bg-white" className="w-5 h-5 mt-20" />;
+  if (loader || !qrCodeDataUrl) return <Loader innerBg="bg-white" className="w-5 h-5 mt-20" />;
   if (!templateContent) return null;
 
   try {
@@ -53,14 +74,22 @@ const VcSvgTemplate = ({ vc, templateUrl, onError }: VcSvgTemplateProps) => {
       }
     );
 
-    let renderedSvg = Mustache.render(preprocessedTemplate, vc);
+    const renderedVc = {
+      ...vc,
+      qrCodeImage: qrCodeDataUrl
+    };
+    
+    let renderedSvg = Mustache.render(preprocessedTemplate, renderedVc);
+    
     renderedSvg = DOMPurify.sanitize(renderedSvg, {
       USE_PROFILES: { svg: true, svgFilters: true },
-      ADD_TAGS: ["use"],
-      ADD_ATTR: ["target"],
+      ADD_TAGS: ["use", "image"],
+      ADD_ATTR: ["target", "href", "xlink:href", "preserveAspectRatio", "x", "y", "width", "height", "id"],
+      ALLOW_DATA_ATTR: true,
       FORBID_TAGS: ["script", "iframe", "object", "embed"],
       FORBID_ATTR: ["onerror", "onload", "onclick", "onmouseover"],
     });
+    
     return (
       <div className="w-full flex justify-center items-center">
         <div dangerouslySetInnerHTML={{ __html: renderedSvg }} />
