6a68adeaa5a9ca97c1308a100034708234d621e9
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-02-18 18:21:42 +0530

    [INJIVER-1439] - add responseCode, responseCodeExpiryAt, responseCodeUsed in entity
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/models/VPSubmission.java
+++ verify-service/src/main/java/io/inji/verify/models/VPSubmission.java
@@ -2,22 +2,23 @@ package io.inji.verify.models;
 
 import io.inji.verify.serialization.impl.PresentationSubmissionDtoConverter;
 import jakarta.persistence.*;
+import org.hibernate.annotations.Check;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nimbusds.jose.shaded.gson.annotations.SerializedName;
-
 import io.inji.verify.dto.submission.PresentationSubmissionDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import java.sql.Timestamp;
 
 @Table(name = "vp_submission")
 @Getter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
+@Check(constraints = "response_code IS NULL OR (response_code_expiry_at IS NOT NULL AND response_code_used IS NOT NULL)")
 public class VPSubmission {
     @Id
     @JsonProperty("state")
@@ -35,4 +36,10 @@ public class VPSubmission {
     private final String error;
 
     private final String errorDescription;
+
+    private final String responseCode;
+
+    private final Timestamp responseCodeExpiryAt;
+
+    private final Boolean responseCodeUsed;
 }
