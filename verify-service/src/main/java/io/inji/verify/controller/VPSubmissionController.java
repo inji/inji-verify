930385e830034ec60e5684829485a44d7232ef5c
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-02-18 18:10:49 +0530

    [INJIVER-1439] - send response for same-device flow
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/controller/VPSubmissionController.java
+++ verify-service/src/main/java/io/inji/verify/controller/VPSubmissionController.java
@@ -1,15 +1,27 @@
 package io.inji.verify.controller;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Set;
-
+import java.util.Map;
+import java.util.UUID;
+import java.util.Objects;
+import java.util.HashMap;
 import com.nimbusds.jose.shaded.gson.JsonSyntaxException;
+import io.inji.verify.dto.core.ErrorDto;
+import io.inji.verify.enums.ErrorCode;
+import io.inji.verify.exception.VPSubmissionException;
+import io.inji.verify.models.AuthorizationRequestCreateResponse;
+import io.inji.verify.repository.AuthorizationRequestCreateResponseRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import com.nimbusds.jose.shaded.gson.Gson;
 import io.inji.verify.dto.authorizationrequest.VPRequestStatusDto;
 import io.inji.verify.dto.submission.PresentationSubmissionDto;
@@ -23,6 +35,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping(path = Constants.RESPONSE_SUBMISSION_URI_ROOT)
@@ -32,16 +45,25 @@ public class VPSubmissionController {
     @Value("${inji.verify.redirect-uri:#{null}}")
     String redirectUri;
 
+    @Value("${inji.verify.responce-code-expiry-time:#{5}}")
+    int responseCodeExpiryTime;
+
+    @Value("${inji.verify.validate-response-code-with-time:#{true}}")
+    boolean validateResponseCodeWithTime;
+
     final VerifiablePresentationRequestService verifiablePresentationRequestService;
 
     final VerifiablePresentationSubmissionService verifiablePresentationSubmissionService;
 
     final Gson gson;
 
-    public VPSubmissionController(VerifiablePresentationRequestService verifiablePresentationRequestService, VerifiablePresentationSubmissionService verifiablePresentationSubmissionService, Gson gson) {
+    final AuthorizationRequestCreateResponseRepository authorizationRequestCreateResponseRepository;
+
+    public VPSubmissionController(VerifiablePresentationRequestService verifiablePresentationRequestService, VerifiablePresentationSubmissionService verifiablePresentationSubmissionService, Gson gson, AuthorizationRequestCreateResponseRepository authorizationRequestCreateResponseRepository) {
         this.verifiablePresentationRequestService = verifiablePresentationRequestService;
         this.verifiablePresentationSubmissionService = verifiablePresentationSubmissionService;
         this.gson = gson;
+        this.authorizationRequestCreateResponseRepository = authorizationRequestCreateResponseRepository;
     }
 
     @PostMapping(path = Constants.RESPONSE_SUBMISSION_URI, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
@@ -66,8 +88,7 @@ public class VPSubmissionController {
 
         // --- 3. Check if error present ---
         if (error != null) {
-            VPSubmissionDto vpSubmissionDto = new VPSubmissionDto(null, null, state, error, errorDescription);
-            verifiablePresentationSubmissionService.submit(vpSubmissionDto);
+            processVPSubmission(null, state, null, error, errorDescription, null, null);
             return new ResponseEntity<>(HttpStatus.OK);
         } else {
             // --- 4. Presentation Submission Validation ---
@@ -79,17 +100,43 @@ public class VPSubmissionController {
                     return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(violationMessage);
                 }
 
-                VPSubmissionDto vpSubmissionDto = new VPSubmissionDto(vpToken, presentationSubmissionDto, state, null, null);
-                verifiablePresentationSubmissionService.submit(vpSubmissionDto);
+                AuthorizationRequestCreateResponse authorizationRequestCreateResponse = authorizationRequestCreateResponseRepository.findById(state).orElse(null);
                 Map<String, Object> response = new HashMap<>();
-                if (StringUtils.hasText(redirectUri)) {
-                    response.put("redirect_uri", redirectUri);
+                if (authorizationRequestCreateResponse != null) {
+                    String presentationFlow = authorizationRequestCreateResponse.getAuthorizationDetails().getPresentationFlow();
+                    if (StringUtils.hasText(redirectUri) && Objects.equals(presentationFlow, "same_device")) {
+                        String responseCode = UUID.randomUUID().toString();
+                        Timestamp expiryAt = Timestamp.from(Instant.now().plus(responseCodeExpiryTime, ChronoUnit.MINUTES));
+                        String updatedRedirectUri = getUpdatedRedirectUri(responseCode);
+                        response.put("redirect_uri", updatedRedirectUri);
+                        processVPSubmission(vpToken, state, presentationSubmissionDto, null,null, responseCode, expiryAt);
+                    }
+                } else {
+                    processVPSubmission(vpToken, state, presentationSubmissionDto, null, null, null, null);
                 }
                 return ResponseEntity.status(HttpStatus.OK).body(response);
             } catch (JsonSyntaxException e) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("INVALID_PRESENTATION_SUBMISSION");
+            } catch (VPSubmissionException e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorDto(ErrorCode.VP_SUBMISSION_EXCEPTION));
             }
         }
+    }
+
+    private String getUpdatedRedirectUri(String responseCode) {
+        return UriComponentsBuilder
+                .fromUriString(redirectUri)
+                .queryParam("response_code", responseCode)
+                .build()
+                .toUriString();
+    }
+
+    private void processVPSubmission(String vpToken, String state, PresentationSubmissionDto presentationSubmissionDto, String error, String errorDescription, String responseCode, Timestamp responseCodeExpiryAt) {
+        if (validateResponseCodeWithTime && responseCode != null && responseCodeExpiryAt == null) {
+            throw new VPSubmissionException();
+        }
+        VPSubmissionDto vpSubmissionDto = new VPSubmissionDto(vpToken, presentationSubmissionDto, state, error, errorDescription, responseCode, responseCodeExpiryAt, false);
+        verifiablePresentationSubmissionService.submit(vpSubmissionDto);
     }
 
     private boolean isValidResponse(String vpToken, String error, String presentationSubmission) {
@@ -103,5 +150,4 @@ public class VPSubmissionController {
 
         return hasValidVpBlock || hasValidErrorBlock;
     }
-
 }
