cfdd2ad7197e8c20e6fb869dd24da4c6367cdf5a
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-02-20 14:40:10 +0530

    [INJIVER-1439] - add setter
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/dto/submission/VPSubmissionDto.java
+++ verify-service/src/main/java/io/inji/verify/dto/submission/VPSubmissionDto.java
@@ -4,10 +4,14 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import java.sql.Timestamp;
 
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class VPSubmissionDto {
     String vpToken;
     @Valid
