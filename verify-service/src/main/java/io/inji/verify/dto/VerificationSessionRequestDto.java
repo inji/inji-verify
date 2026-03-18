a1adb4d537bb4edce04180f6c586e41eea2bb96d
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-03-18 12:21:29 +0530

    [INJIVER-1580] - updated dto
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/dto/VerificationSessionRequestDto.java
+++ verify-service/src/main/java/io/inji/verify/dto/VerificationSessionRequestDto.java
@@ -1,8 +1,6 @@
 package io.inji.verify.dto;
 
 import io.inji.verify.dto.result.VerificationRequestDto;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,8 +12,6 @@ import java.util.List;
 @Getter
 @Setter
 public class VerificationSessionRequestDto extends VerificationRequestDto {
-    @NotEmpty
-    @NotNull
     private String responseCode;
 
     public VerificationSessionRequestDto(boolean skipStatusChecks, List<String> statusCheckFilters, boolean includeClaims, String responseCode) {
