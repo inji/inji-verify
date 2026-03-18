85b19f0c227d9d74d80194eac6448090fd6dce65
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-03-18 11:20:04 +0530

    [INJIVER-1580] - add notNull and notEmpty annotation
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/dto/VerificationSessionRequestDto.java
+++ verify-service/src/main/java/io/inji/verify/dto/VerificationSessionRequestDto.java
@@ -1,6 +1,8 @@
 package io.inji.verify.dto;
 
 import io.inji.verify.dto.result.VerificationRequestDto;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,5 +13,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class VerificationSessionRequestDto extends VerificationRequestDto {
+    @NotEmpty
+    @NotNull
     private String responseCode;
 }
