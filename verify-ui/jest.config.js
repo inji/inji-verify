58cdc67df44fd2a3702314640e477bf6b1c8e854
srikanth716 <srikanthsri7447@gmail.com>
2026-03-24 17:46:12 +0530

    [INJIVER-1596] fix:node compatability issue for unit tests
    Signed-off-by: srikanth716 <srikanthsri7447@gmail.com>

--- verify-ui/jest.config.js
+++ verify-ui/jest.config.js
@@ -5,10 +5,12 @@ module.exports = {
   rootDir: './',
   testPathIgnorePatterns: ["<rootDir>/node_modules"],
   testMatch: ["<rootDir>/src/__tests__/**/*.spec.ts", "<rootDir>/src/__tests__/**/*.spec.tsx", "<rootDir>/src/__tests__/**/*.spec.js"],
+  setupFiles: ["<rootDir>/src/polyfills/stringReplaceAll.ts"],
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   extensionsToTreatAsEsm: [".ts", ".tsx"],
   transform: {
     '^.+\\.m?[tj]sx?$': ['ts-jest', { useESM: true }],
+    '^.+\\.cjs$': 'babel-jest',
     "\\.svg$": "<rootDir>/__mocks__/svgFileTransformer.js",
     '.+\\.(png|jpg|jpeg)$': "<rootDir>/__mocks__/svgFileTransformer.js"
   },
