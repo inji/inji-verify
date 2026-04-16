b45bb404f459f10721f783149830724a7a7f5b21
Abhi <abhishek.shankarcs@gmail.com>
2026-04-16 15:09:23 +0530

    [MOSIP-44572] Updated script to support Web Wallet configuration - release-0.18.x
    Signed-off-by: Abhi <abhishek.shankarcs@gmail.com>

--- deploy/inji-verify-ui/install.sh
+++ deploy/inji-verify-ui/install.sh
@@ -7,7 +7,7 @@ if [ $# -ge 1 ] ; then
 fi
 
 NS=injiverify
-CHART_VERSION=release-0.18.x
+CHART_VERSION=0.18.0-develop
 
 DEFAULT_INJIVERIFY_HOST=$( kubectl get cm inji-stack-config -n config-server -o jsonpath={.data.injiverify-host} )
 # Check if INJIVERIFY_HOST is present under configmap/inji-stack-config of configserver
