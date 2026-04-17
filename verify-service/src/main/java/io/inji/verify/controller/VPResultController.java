3be14ecce6655b67057d8744cf66dc090d42aea5
sarupmoni <93664377+sarupmoni@users.noreply.github.com>
2026-04-17 10:28:57 +0530

    [INJIVER-1614] - handle ClientIdNonceException InvalidVpTokenException
    Signed-off-by: sarupmoni <93664377+sarupmoni@users.noreply.github.com>

--- verify-service/src/main/java/io/inji/verify/controller/VPResultController.java
+++ verify-service/src/main/java/io/inji/verify/controller/VPResultController.java
@@ -21,19 +21,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.MissingRequestCookieException;
 import java.util.List;
 import static io.inji.verify.shared.Constants.COOKIE_NAME;
 
 @RestController
 @Slf4j
+@CrossOrigin(origins = "http://loclhost:3000", allowCredentials = "true")
 public class VPResultController {
 
     @Value("${inji.verify.cookie-secure-value:#{true}}")
