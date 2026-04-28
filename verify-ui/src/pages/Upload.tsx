66bd77efdd2fbe8ec3e5ceffacb21b45b9ceb164
Muskan Rai <184902125+muskanrai-tw@users.noreply.github.com>
2026-04-28 16:08:03 +0530

    INJIVER-1621-updated
    Signed-off-by: Muskan Rai <184902125+muskanrai-tw@users.noreply.github.com>

--- verify-ui/src/pages/Upload.tsx
+++ verify-ui/src/pages/Upload.tsx
@@ -10,8 +10,8 @@ import { raiseAlert } from "../redux/features/alerts/alerts.slice";
 import { useAppDispatch } from "../redux/hooks";
 import { QRCodeVerification } from "@injistack/react-inji-verify-sdk";
 import { getClientId, isVPSubmissionSupported, vcVerificationV2Request,} from "../utils/commonUtils";
-import {updateInternetConnectionStatus} from "../redux/features/application-state/application-state.slice";
 import {checkInternetStatus} from "../utils/misc";
+import {updateInternetConnectionStatus} from "../redux/features/application-state/application-state.slice";
 
 export const Upload = () => {
   const { t } = useTranslation("Upload");
@@ -24,35 +24,9 @@ export const Upload = () => {
             <div className="absolute top-[58px] left-[98px] lg:top-[165px] lg:left-[50%] lg:translate-x-[-50%] lg:translate-y-[-50%]">
                 <QrIcon className="w-[78px] lg:w-[100px]" />
             </div>
-          <button
-              className="absolute top-[130px] left-[45px] lg:top-[200px] lg:left-[95px] cursor-pointer"
-              onClick={(e) => {
-                  e.preventDefault();
-                  e.stopPropagation();
-
-                  document.getElementById("upload-qr")?.click();
-
-                  (async () => {
-                      const isOnline = await checkInternetStatus();
-
-                      dispatch(
-                          updateInternetConnectionStatus({
-                              internetConnectionStatus: isOnline ? "ONLINE" : "OFFLINE",
-                          })
-                      );
-                  })();
-              }}
-          >
-                <UploadQrCode
-                        className="pointer-events-none"
-                        displayMessage={t("Common:Button.upload")} />
-            </button>
-            <button
-                id="trigger-upload"
-                className="hidden"
-                onClick={() => {
-                    document.getElementById("upload-qr")?.click();
-                }}
+            <UploadQrCode
+                className="absolute top-[130px] left-[45px] lg:top-[200px] lg:left-[95px]"
+                displayMessage={t("Common:Button.upload")}
             />
         </div>
     );
@@ -72,44 +46,53 @@ const handleOnVCProcessed = (data: any[]) => {
         );
 };
 
-  return (
+return (
     <div className="flex flex-col pt-0 pb-[100px] lg:py-[42px] px-0 lg:px-[104px] text-center content-center justify-center">
-      <div className="xs:col-end-13">
-        <div
-          className={`relative grid content-center justify-center w-[275px] h-auto lg:w-[360px] aspect-square my-1.5 mx-auto bg-cover`}
-          style={{ backgroundImage: `url(${ScanOutline})` }}
-        >
-          <QRCodeVerification
-            triggerElement={triggerElement}
-            verifyServiceUrl={ window._env_.VERIFY_SERVICE_API_URL}
-            isEnableScan={false}
-            onVCProcessed={handleOnVCProcessed}
-            uploadButtonId={"upload-qr"}
-            uploadButtonStyle="hidden"
-            onError={(error) => {
-              dispatch(
-                raiseAlert({
-                  message: error.message,
-                  severity: "error",
-                  open: true,
-                })
-              );
-              dispatch(goToHomeScreen({}));
-            }}
-            clientId={getClientId()}
-            isVPSubmissionSupported={isVPSubmissionSupported()}
-            vcVerificationV2Request ={vcVerificationV2Request}
-          />
+        <div className="xs:col-end-13">
+            <div
+                className={`relative grid content-center justify-center w-[275px] h-auto lg:w-[360px] aspect-square my-1.5 mx-auto bg-cover`}
+                style={{ backgroundImage: `url(${ScanOutline})` }}
+            >
+                <QRCodeVerification
+                    triggerElement={triggerElement}
+                    verifyServiceUrl={ window.location.origin + window._env_.VERIFY_SERVICE_API_URL}
+                    isEnableScan={false}
+                    onVCProcessed={handleOnVCProcessed}
+                    uploadButtonId={"upload-qr"}
+                    uploadButtonStyle="hidden"
+                    onError={async (error) => {
+                        let isOnline = await checkInternetStatus();
+                        dispatch(
+                            updateInternetConnectionStatus({
+                                internetConnectionStatus: isOnline ? "ONLINE" : "OFFLINE",
+                            })
+                        );
+                        if (isOnline) {
+                            document.getElementById("trigger-scan")?.click();
+                            dispatch(
+                                raiseAlert({
+                                    message: error.message,
+                                    severity: "error",
+                                    open: true,
+                                })
+                            );
+                        }
+                        dispatch(goToHomeScreen({}));
+                    }}
+                    clientId={getClientId()}
+                    isVPSubmissionSupported={isVPSubmissionSupported()}
+                    vcVerificationV2Request ={vcVerificationV2Request}
+                />
+            </div>
+                <div className="grid text-center content-center justify-center pt-2">
+                    <p
+                        id="file-format-constraints"
+                        className="font-normal text-normalTextSize text-uploadDescription w-[280px]"
+                    >
+                        {t("format")}
+                    </p>
+                </div>
         </div>
-        <div className="grid text-center content-center justify-center pt-2">
-          <p
-            id="file-format-constraints"
-            className="font-normal text-normalTextSize text-uploadDescription w-[280px]"
-          >
-            {t("format")}
-          </p>
-        </div>
-      </div>
     </div>
   );
 };
