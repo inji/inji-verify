d6c71b33f48e0778754481df3c08331b54e1b0b9
mayuradesh <mayura.deshmukh@gmail.com>
2026-05-18 18:37:32 +0530

    fixed
    Signed-off-by: mayuradesh <mayura.deshmukh@gmail.com>

--- db_upgrade_script/inji_verify/sql/0.18.0_to_0.18.1_rollback.sql
+++ db_upgrade_script/inji_verify/sql/0.18.0_to_0.18.1_rollback.sql
@@ -3,7 +3,7 @@
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 -- -------------------------------------------------------------------------------------------------
--- Rollback Script: v0.18.0 to v0.18.1
+-- Rollback Script: v0.18.1 to v0.18.0
 -- Database       : inji_verify
 -- Purpose        : Revert schema changes introduced in version 0.18.1
 -- -------------------------------------------------------------------------------------------------
